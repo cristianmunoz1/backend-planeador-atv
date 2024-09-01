a1a628b70f737daa12f3c3340966b533399bda66
Brandon Duque <brandugar@gmail.com>
2024-09-01 11:47:31 -0500

    Se agrega el proyecto al repositorio

---
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/ProyectoIntegradorIApplication.java
@@ -0,0 +1,13 @@
+package com.proyIntUdeA.proyectoIntegradorI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyIntUdeA/proyectoIntegradorI/ProyectoIntegradorIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyIntUdeA.proyectoIntegradorI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
