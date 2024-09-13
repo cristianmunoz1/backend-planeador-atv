3a4ad739075dcff7b796b2b5d9730dc181490afd
cristianmunoz1 <cristian.munoz3@udea.edu.co>
2024-09-13 10:44:34 -0500

    Se crea la entidad de Degree

---
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/entity/DegreeEntity.java
@@ -0,0 +1,24 @@
+package com.proyIntUdeA.proyectoIntegradorI.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "degree")
+public class DegreeEntity {
+    @Id
+    public String degree_id;
+    public String degree_name;
+    public String degree_modality;
+    public String degree_department;
+}
